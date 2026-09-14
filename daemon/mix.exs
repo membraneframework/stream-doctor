7e3b41f488fa7feeecacd793203d40c059a66102
Łukasz Kita <lukasz.kita0@gmail.com>
2026-09-14 18:01:47 +0200

    Bump to v0.0.4

--- daemon/mix.exs
+++ daemon/mix.exs
@@ -6,7 +6,7 @@ defmodule StreamDoctor.MixProject do
   def project do
     [
       app: :stream_doctor,
-      version: "0.1.0",
+      version: "0.0.4",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
